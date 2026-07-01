5bc5455b053837febd92da5ed205205ebc2d7144
Navya4301 <manjulaa4819@gmail.com>
2026-07-01 14:46:06 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+script.js
+        function SelectElements() 
+        {
+            let titleElement = document.getElementById("title");
+            title js.style.color="blue";
+            let message js = document.get elementsBy class name("message")[0];
+            message js class= style.color="green";
+            let paragraph = document.get ElementsByTagname("p");
+            paragraph[1].style.background color="yellow";
+            let elements = document.querySelectorAll('h1, p')
+        }
+    
